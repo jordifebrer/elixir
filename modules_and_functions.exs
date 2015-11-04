0e51c1f68e350d384b9c205221c1f3e5fd75684a
Jordi Febrer <jordi.febrer@gmail.com>
2015-11-04 00:09:10 +0000

    Added sum and gdc functions

--- modules_and_functions.exs
+++ modules_and_functions.exs
@@ -5,6 +5,14 @@ defmodule ModulesAndFunctions do
   def double(n), do: n * 2
   # Exercise: ModulesAndFunctions-1
   def triple(n), do: n * 3
-  # Exercise: ModulesAndFunctions-1
+  # Exercise: ModulesAndFunctions-3
   def quadruple(n), do: double(n) * 2
+  # Exercise: ModulesAndFunctions-4
+  def sum(0), do: 0
+  def sum(n) when n > 0 do
+    n + sum(n - 1)
+  end
+  # Exercise: ModulesAndFunctions-5
+  def gdc(x, 0), do: x
+  def gdc(x, y), do: gdc(y, rem(x, y))
 end
